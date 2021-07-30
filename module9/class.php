4be40e5a04895e3ea1f55dcc801776414afc39c4
Azman Zakaria <azman1204@yahoo.com>
2021-07-30 17:02:33 +0800

    oop

--- module9/class.php
+++ module9/class.php
@@ -5,9 +5,14 @@ class Fruit {
   public $name = 'Jambu Batu';
   public $color = 'Hijau';
 
-  // ada 2 kali _
+  // ada 2 kali _. parameter boleh 0 OR many
   function __construct($name) {
-    $this->name = $name;
+    $this->name= $name;
+  }
+
+  // auto run just before obj di destroy automatically
+  function __destruct() {
+    echo "The fruit is {$this->Name}.";
   }
 
   // Methods
@@ -23,14 +28,15 @@ class Fruit {
 // objet
 $jambu = new Fruit('Jambu batu');
 echo $jambu->getName(); // output : Jambu batu
+var_dump($jambu);
 
 $apple = new Fruit('Apple');
 $banana = new Fruit('Banana');
 $apple->setName('Apple');
 $banana->setName('Banana');
 
-echo $apple->getName();
+echo $apple->getName();// output : Apple
 echo "<br>";
-echo $banana->getName();
-// output : Apple<br>Banana
+echo $banana->getName();// output: Banana
+// output : The fruit is Banana.The fruit is Apple.The fruit is Jambu batu.
 ?>
